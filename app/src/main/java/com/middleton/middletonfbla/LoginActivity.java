e0191154d28d62c4c63dd9d66ef5558bb977540a
hamzachouaibi <hamzaburger123@hotmail.com>
2019-11-16 12:36:57 -0500

    Work

---
+++ app/src/main/java/com/middleton/middletonfbla/LoginActivity.java
@@ -0,0 +1,71 @@
+package com.middleton.middletonfbla;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
+    TextInputEditText emailET, passET;
+    Button loginBtn;
+    String email, password;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        firebaseAuth = FirebaseAuth.getInstance();
+        
+        emailET = (TextInputEditText) findViewById(R.id.loginEmail);
+        passET = (TextInputEditText) findViewById(R.id.loginPass);
+        loginBtn = (Button) findViewById(R.id.loginBtn);
+        
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = emailET.getText().toString().trim();
+                password = passET.getText().toString().trim();
+
+                if (password.length()<6){
+                    passET.setError("Password needs to be 6 characters or longer");
+                    return;
+                }else if (password.isEmpty()){
+                    passET.setError("Need a password to continue");
+                    return;
+                }else if (email.isEmpty()){
+                    emailET.setError("Need an Email to continue");
+                    return;
+                }
+                
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        Toast.makeText(LoginActivity.this, authResult.getUser().getUid(), Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                
+            }
+        });
+        
+        
+    }
+}
