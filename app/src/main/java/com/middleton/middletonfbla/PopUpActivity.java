4309feea73348e634aef8fdbc487c501e4d16bcb
hamzachouaibi <hamzaburger123@hotmail.com>
2019-12-14 17:09:09 -0500

    work on home page

---
+++ app/src/main/java/com/middleton/middletonfbla/PopUpActivity.java
@@ -0,0 +1,36 @@
+package com.middleton.middletonfbla;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.WindowManager;
+
+public class PopUpActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pop_up);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width * .8), (int)(height * .7));
+
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.gravity = Gravity.CENTER;
+        params.x = 0;
+        params.y = -20;
+
+        getWindow().setAttributes(params);
+
+    }
+}
--- app/src/main/java/com/middleton/middletonfbla/Register/RegisterCompetition.java
+++ app/src/main/java/com/middleton/middletonfbla/Register/RegisterCompetition.java
@@ -1,16 +1,74 @@
 package com.middleton.middletonfbla.Register;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.middleton.middletonfbla.PopUpActivity;
 import com.middleton.middletonfbla.R;
 
-public class RegisterCompetition extends AppCompatActivity {
+public class RegisterCompetition extends AppCompatActivity implements View.OnClickListener {
+
+    CardView objective, speaking, design, hybrid, writing;
+    FloatingActionButton registerCompBtn;
+    FirebaseAuth auth;
+    DocumentReference documentReference;
+    String competition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_competition);
+        auth = FirebaseAuth.getInstance();
+        documentReference = FirebaseFirestore.getInstance().collection("User_Information").document(auth.getCurrentUser().getUid());
+
+        objective = (CardView) findViewById(R.id.objectiveCardview);
+        speaking = (CardView) findViewById(R.id.speakingCardview);
+        design = (CardView) findViewById(R.id.designCardview);
+        hybrid = (CardView) findViewById(R.id.hybridCardview);
+        writing = (CardView) findViewById(R.id.writingCardview);
+        registerCompBtn = (FloatingActionButton) findViewById(R.id.registerComp);
+
+        objective.setOnClickListener(this);
+        speaking.setOnClickListener(this);
+        design.setOnClickListener(this);
+        hybrid.setOnClickListener(this);
+        writing.setOnClickListener(this);
+
+
+        registerCompBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                documentReference.update("competition", competition);
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == hybrid){
+            startActivity(new Intent(RegisterCompetition.this, PopUpActivity.class));
+        }
+        if(v == objective){
+            startActivity(new Intent(RegisterCompetition.this, PopUpActivity.class));
+        }
+        if (v == speaking){
+            startActivity(new Intent(RegisterCompetition.this, PopUpActivity.class));
+        }
+        if (v == design){
+            startActivity(new Intent(RegisterCompetition.this, PopUpActivity.class));
+        }
+        if (v == writing){
+            startActivity(new Intent(RegisterCompetition.this, PopUpActivity.class));
+        }
     }
 }
